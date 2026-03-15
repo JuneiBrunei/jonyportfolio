4cd3b64bc1a8f5bbb254a9ccb7597562480d8b79
Zhangir Yerkassym <zhangiryerkassym@MacBook-Air-Zhangir.local>
2026-03-15 23:00:20 +0100

    update favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,6 +27,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        {/* Указываем favicon */}
+        <link rel="icon" href="/favicon.ico" />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
